3cb78ef2db180c242de83903263c0c187982108b
Jack Lacey-Rogers <jacklaceyrogers6@gmail.com>
2023-04-07 21:28:37 -0400

    added html stucture, bootstrap layout, css styleing and js variables to create functions later

---
+++ main.js
@@ -0,0 +1,12 @@
+const apiKey = '8730129a02fdec31293217d5a8a9c63a';
+
+// Retrieve elements from the DOM and create variables
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const searchHistory = document.getElementById('searchHistory');
+const cityName = document.getElementById('cityName');
+const temperature = document.getElementById('temperature');
+const description = document.getElementById('description');
+
+
+// Create a function to retrieve the weather data from the API
