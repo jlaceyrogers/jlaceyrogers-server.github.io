07a3446b347b29d1c4889ae77c9d19a602135c75
Jack Lacey-Rogers <jacklaceyrogers6@gmail.com>
2023-04-16 14:49:29 -0400

    added boxes for 5 day forecast

--- main.js
+++ main.js
@@ -1,33 +1,54 @@
 const apiKey = '8730129a02fdec31293217d5a8a9c63a';
+//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 
 // Retrieve elements from the DOM and create variables
+
 const searchInput = document.getElementById('searchInput');
 const searchHistory = document.getElementById('searchHistory');
 const cityName = document.getElementById('cityName');
 const temperature = document.getElementById('temperature');
 const description = document.getElementById('description');
+const submitBTN = document.getElementById('searchButton')
+
+var getWeatherData;
+
 var lat = ("latitude");
 var lon = ("longitude");
 
 
+window.addEventListener("DOMContentLoaded", function(){
+        
+        submitBTN.addEventListener("click", function(){
+            var searchedCity = searchInput.value
+            //console.log(searchedCity)
+            getWeatherData(searchedCity)
+        })
 
+        cityName.value = getWeatherData.city.name;
 
+});
 
+function getWeatherData(city){
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
-// Create a function to retrieve the weather data from the API
-const getWeather = (city) => {
-    const url = `api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={8730129a02fdec31293217d5a8a9c63a}`;
-    
-    // select the search button element and attach a click event listener
-    const searchButton = document.getElementById('searchButton');
-    searchButton.addEventListener('click', () => {
-        // get the city name from the input field
-        const city = document.getElementById('searchInput').value;
+    fetch(apiUrl)
+      .then(function (response) {
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function (data) {
+            console.log(data);
+            getWeatherData = data;
+            console.log(getWeatherData);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to GitHub');
+      });
+        
+}
 
-        // replace the latitude, longitude, and API key placeholders in the URL with actual values
-        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={8730129a02fdec31293217d5a8a9c63a}`;
-
-    });
-};
-
-    // this function will be called when the search button is clicked
